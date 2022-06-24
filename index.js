52b570bb6af93baff21aee2cab7caa416b711609
BlackErik <blac.erik@gmail.com>
2022-06-24 09:41:35 -0600

    server listening

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ flags.parse();
 
 const dotenv = require("dotenv");
 const port = flags.get("port") || process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
